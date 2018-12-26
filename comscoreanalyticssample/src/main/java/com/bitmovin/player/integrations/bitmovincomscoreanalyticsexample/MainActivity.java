863d666f8c5faa5d56b2e817ca24788b4cf1f35e
Cory Zachman <cory.zachman@bitmovin.com>
2018-12-26 16:17:46 -0600

    pause / resume on background of the video player

--- comscoreanalyticssample/src/main/java/com/bitmovin/player/integrations/bitmovincomscoreanalyticsexample/MainActivity.java
+++ comscoreanalyticssample/src/main/java/com/bitmovin/player/integrations/bitmovincomscoreanalyticsexample/MainActivity.java
@@ -89,6 +89,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (bitmovinPlayer != null && !bitmovinPlayer.isAd()) {
+            this.bitmovinPlayerView.onResume();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (bitmovinPlayer != null && !bitmovinPlayer.isAd()) {
+            this.bitmovinPlayerView.onPause();
+        }
+    }
+
     private void loadLive() {
         SourceItem sourceItem = new SourceItem(new HLSSource("https://bitdash-a.akamaihd.net/content/sintel/hls/playlist.m3u8"));
         SourceConfiguration sourceConfig = new SourceConfiguration();
