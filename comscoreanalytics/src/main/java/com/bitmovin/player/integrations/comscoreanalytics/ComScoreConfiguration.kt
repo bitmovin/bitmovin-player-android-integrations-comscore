93c6c7d0f38cef25adc8341575f639c09fa8ec79
nsc1234 <nsc9012@gmail.com>
2019-10-22 16:12:25 -0600

    Convert ComScoreConfiguration to Kotlin

---
+++ comscoreanalytics/src/main/java/com/bitmovin/player/integrations/comscoreanalytics/ComScoreConfiguration.kt
@@ -0,0 +1,7 @@
+package com.bitmovin.player.integrations.comscoreanalytics
+
+class ComScoreConfiguration(
+    val publisherId: String,
+    val publisherSecret: String,
+    val applicationName: String
+)
